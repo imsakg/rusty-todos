7f03a0726bcd3fff0715f42d7599c1762746ad44
msa <msas@tuta.io>
2022-11-03 23:08:15 +0300

    test: model_todo unittest.

---
+++ backend/src/_tests/model_todo.rs
@@ -0,0 +1,28 @@
+use crate::model::db::init_db;
+
+use super::TodoMac;
+
+#[tokio::test]
+
+async fn model_todo_list() -> Result<(), Box<dyn std::error::Error>> {
+	// -- FIXTURE
+	let db = init_db().await?;
+
+	// -- ACTION
+	let todos = TodoMac::list(&db).await?;
+
+	// -- CHECK
+	assert_eq!(2, todos.len());
+	// todo 101
+	assert_eq!(101, todos[0].id);
+	assert_eq!(123, todos[0].cid);
+	assert_eq!("todo 101", todos[0].title);
+	// todo.100
+	assert_eq!(100, todos[1].id);
+	assert_eq!(123, todos[1].cid);
+	assert_eq!("todo 100", todos[1].title);
+
+	println!("\n\n->> {:?}", todos);
+
+	Ok(())
+}
