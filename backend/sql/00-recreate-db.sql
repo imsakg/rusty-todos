d0a9b31fdcc916fd5b1f2537f689300f52ff5887
msa <msas@tuta.io>
2022-11-03 23:07:54 +0300

    fix: micro fix for exist tables, types!

--- backend/sql/00-recreate-db.sql
+++ backend/sql/00-recreate-db.sql
@@ -2,6 +2,9 @@
 DROP DATABASE IF EXISTS app_db;
 DROP USER IF EXISTS app_user;
 
+DROP TABLE todo;
+DROP TYPE todo_status_enum CASCADE;
+
 -- DEV ONLY - for quick iteration
 CREATE USER app_user PASSWORD 'app_pwd_to_change';
 CREATE DATABASE app_db owner app_user ENCODING = 'UTF-8';
--- backend/sql/01-create-scheme.sql
+++ backend/sql/01-create-scheme.sql
@@ -1,6 +1,5 @@
 -- TODO Status enum
-
-CREATE TYPE todo_status_enum as ENUM (
+CREATE TYPE todo_status_enum AS ENUM (
     'open',
     'close'
 );
@@ -9,8 +8,8 @@ CREATE TYPE todo_status_enum as ENUM (
 CREATE TABLE todo (
     id bigserial,
     cid bigint NOT NULL, -- creator userID
-    ctime timestamp WITH time zone DEFAULT now(),
-    title text NOT NULL;
+    ctime timestamp with time zone DEFAULT now(),
+    title text NOT NULL,
     status todo_status_enum NOT NULL DEFAULT 'open'
 ); 
 ALTER SEQUENCE todo_id_seq RESTART WITH 1000;
