20c1ce7725f6af9604625eb87e8c0e05aca1ee99
msa <msas@tuta.io>
2022-11-04 22:26:50 +0300

    init: module for web layer.

---
+++ backend/src/web/mod.rs
@@ -0,0 +1,32 @@
+use crate::model::Db;
+use warp::Filter;
+
+use std::{path::Path, sync::Arc};
+
+pub async fn start_web(web_folder: &str, web_port: u16, db: Arc<Db>) -> Result<(), Error> {
+	// Validate web_folder
+	if !Path::new(web_folder).exists() {
+		return Err(Error::FailStartWebFolderNotFound((web_folder.to_string())));
+	}
+
+	// Static content
+	let content = warp::fs::dir(web_folder.to_string());
+	let root_index = warp::get()
+		.and(warp::path::end())
+		.and(warp::fs::file(format!("{}/index.html", web_folder)));
+	let static_site = content.or(root_index);
+
+	// Combine all routes
+	let routes = static_site;
+
+	println!("Start 127.0.0.1:{} at {}", web_port, web_folder);
+	warp::serve(routes).run(([127, 0, 0, 1], web_port)).await;
+
+	Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+	#[error("Web server failed to start because web-folder '{0}' not found!")]
+	FailStartWebFolderNotFound(String),
+}
