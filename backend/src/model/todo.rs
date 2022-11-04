c57f8951e857940f9bed2d412865ac23af50d9a2
msa <msas@tuta.io>
2022-11-04 17:32:26 +0300

    change: left queries for sqlBuilder.

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -1,3 +1,5 @@
+use sqlb::HasFields;
+
 use super::db::Db;
 use crate::model;
 
@@ -10,12 +12,12 @@ pub struct Todo {
 	pub status: TodoStatus,
 }
 
-#[derive(Default, Debug, Clone)]
+#[derive(sqlb::Fields, Default, Debug, Clone)]
 pub struct TodoPatch {
-	pub cid: Option<i64>,
 	pub title: Option<String>,
 	pub status: Option<TodoStatus>,
 }
+
 #[derive(sqlx::Type, Debug, Clone, PartialEq, Eq)]
 #[sqlx(type_name = "todo_status_enum")]
 #[sqlx(rename_all = "lowercase")]
@@ -23,6 +25,9 @@ pub enum TodoStatus {
 	Open,
 	Close,
 }
+
+sqlb::bindable!(TodoStatus);
+
 /* #endregion */
 
 /* #region  TodoMAC */
@@ -30,23 +35,29 @@ pub struct TodoMac;
 
 impl TodoMac {
 	pub async fn create(db: &Db, data: TodoPatch) -> Result<Todo, model::Error> {
-		let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status";
-		let query = sqlx::query_as::<_, Todo>(&sql)
-			.bind(123 as i64)
-			.bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+		// let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status"; // TODO - make it dynamic instead of static
+		// let query = sqlx::query_as::<_, Todo>(&sql)
+		// 	.bind(123 as i64) // FIXME - should come from user context
+		// 	.bind(data.title.unwrap_or_else(|| "untitled".to_string()));
+		let mut fields = data.fields();
+		fields.push(("cid", 123).into());
 
-		let todo = query.fetch_one(db).await?;
+		let sb = sqlb::insert()
+			.table("todo")
+			.data(fields)
+			.returning(&["id", "cid", "title", "status"]);
+
+		let todo = sb.fetch_one(db).await?;
 
 		Ok(todo)
 	}
 
 	pub async fn list(db: &Db) -> Result<Vec<Todo>, model::Error> {
-		let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+		// let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
+		let sb = sqlb::select().table("todo").columns(&["id", "cid", "title", "status"]);
 
-		// build the sqlx-query
-		let query = sqlx::query_as::<_, Todo>(&sql);
 		// execute the query
-		let todos = query.fetch_all(db).await?;
+		let todos = sb.fetch_all(db).await?;
 
 		Ok(todos)
 	}
