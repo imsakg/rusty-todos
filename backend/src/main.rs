8d5948e5e80beb32aa3cd1d8fde96251db92298a
msa <msas@tuta.io>
2022-11-04 22:30:28 +0300

    update: init for web server.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,32 @@
 #![allow(unused)]
 
+use std::{env, sync::Arc};
+
+use model::init_db;
+use web::start_web;
+
 mod model;
 mod security;
 mod web;
 
-fn main() {
-    println!("Hi")
+const DEFAULT_WEB_FOLDER: &'static str = "web-folder/";
+const DEFAULT_WEB_PORT: u16 = 8080;
+
+#[tokio::main]
+async fn main() {
+	// compute the web_folder
+	let mut args: Vec<String> = env::args().collect();
+	let web_folder = args.pop().unwrap_or_else(|| DEFAULT_WEB_FOLDER.to_string());
+	let web_port: u16 = DEFAULT_WEB_PORT;
+
+	// get the database
+	// TODO - loop until valid DB
+	let db = init_db().await.expect("Cannot init DB!");
+	let db = Arc::new(db);
+
+	// Start the server
+	match start_web(&web_folder, web_port, db).await {
+		Ok(_) => println!("Server ended!"),
+		Err(ex) => println!("Error  - Web Server failed to start. Cause {:?}", ex),
+	}
 }
