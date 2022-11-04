2647e3c47c63197d227aa907a48d534e5f35bf9b
msa <msas@tuta.io>
2022-11-04 23:05:47 +0300

    exported: todo module.

--- backend/src/web/mod.rs
+++ backend/src/web/mod.rs
@@ -3,6 +3,8 @@ use warp::Filter;
 
 use std::{path::Path, sync::Arc};
 
+mod todo;
+
 pub async fn start_web(web_folder: &str, web_port: u16, db: Arc<Db>) -> Result<(), Error> {
 	// Validate web_folder
 	if !Path::new(web_folder).exists() {
