4e977dd761cc5b51cf7f74cde74ed53e8c85d686
msa <msas@tuta.io>
2022-11-04 20:28:20 +0300

    added: get, update calls!

--- backend/src/model/todo.rs
+++ backend/src/model/todo.rs
@@ -34,6 +34,11 @@ sqlb::bindable!(TodoStatus);
 pub struct TodoMac;
 
 impl TodoMac {
+	const TABLE: &'static str = "todo";
+	const COLUMNS: &'static [&'static str] = &["id", "cid", "title", "status"];
+}
+
+impl TodoMac {
 	pub async fn create(db: &Db, utx: &UserCtx, data: TodoPatch) -> Result<Todo, model::Error> {
 		// let sql = "INSERT INTO todo (cid, title) VALUES ($1, $2) returning id, cid, title, status"; // TODO - make it dynamic instead of static
 		// let query = sqlx::query_as::<_, Todo>(&sql)
@@ -42,11 +47,32 @@ impl TodoMac {
 		let mut fields = data.fields();
 		fields.push(("cid", 123).into());
 
-		let sb = sqlb::insert()
-			.table("todo")
-			.data(fields)
-			.returning(&["id", "cid", "title", "status"]);
+		let sb = sqlb::insert().table(Self::TABLE).data(fields).returning(Self::COLUMNS);
 
+		let todo = sb.fetch_one(db).await?;
+
+		Ok(todo)
+	}
+
+	pub async fn get(db: &Db, _utx: &UserCtx, id: i64) -> Result<Todo, model::Error> {
+		let sb = sqlb::select()
+			.table(Self::TABLE)
+			.columns(Self::COLUMNS)
+			.and_where_eq("id", id);
+		let todo = sb.fetch_one(db).await.map_err(|sqlx_error| match sqlx_error {
+			sqlx::Error::RowNotFound => model::Error::EntityNotFound(Self::TABLE, id.to_string()),
+			other => model::Error::SqlxError(other),
+		})?;
+
+		Ok(todo)
+	}
+
+	pub async fn update(db: &Db, utx: &UserCtx, id: i64, data: TodoPatch) -> Result<Todo, model::Error> {
+		let sb = sqlb::update()
+			.table(Self::TABLE)
+			.data(data.fields())
+			.and_where_eq("id", id)
+			.returning(Self::COLUMNS);
 		let todo = sb.fetch_one(db).await?;
 
 		Ok(todo)
@@ -54,10 +80,7 @@ impl TodoMac {
 
 	pub async fn list(db: &Db, _utx: &UserCtx) -> Result<Vec<Todo>, model::Error> {
 		// let sql = "SELECT id, cid, title, status FROM todo ORDER BY id DESC";
-		let sb = sqlb::select()
-			.table("todo")
-			.columns(&["id", "cid", "title", "status"])
-			.order_by("!id");
+		let sb = sqlb::select().table(Self::TABLE).columns(Self::COLUMNS).order_by("!id");
 
 		// execute the query
 		let todos = sb.fetch_all(db).await?;
