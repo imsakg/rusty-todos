1bb82bd819a2f5174df91cccd0b944d0b0bf83a1
msa <msas@tuta.io>
2022-11-04 20:54:35 +0300

    added: mid, mtime to test script.

--- backend/sql/01-create-scheme.sql
+++ backend/sql/01-create-scheme.sql
@@ -9,6 +9,8 @@ CREATE TABLE todo (
     id bigserial,
     cid bigint NOT NULL, -- creator userID
     ctime timestamp with time zone DEFAULT now(),
+    mid bigint,
+    mtime timestamp with time zone,
     title text NOT NULL,
     status todo_status_enum NOT NULL DEFAULT 'open'
 ); 
