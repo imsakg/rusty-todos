6b3fb4e02c1c94d60fd3598573cc8bddfa9e965a
msa <msas@tuta.io>
2022-11-05 22:40:12 +0300

    test: web_todo_list test is ready.

--- backend/src/_tests/web_todo.rs
+++ backend/src/_tests/web_todo.rs
@@ -1,7 +1,13 @@
-use anyhow::Result;
-use std::sync::Arc;
+use anyhow::{Context, Ok, Result};
+use serde::Deserialize;
+use serde_json::{from_str, from_value, Value};
+use std::{str::from_utf8, sync::Arc};
+use warp::hyper::{body::Bytes, Response};
 
-use crate::{model::init_db, web::todo::todo_rest_filters};
+use crate::{
+	model::{init_db, Todo, TodoStatus},
+	web::todo::todo_rest_filters,
+};
 
 #[tokio::test]
 async fn web_todo_list() -> Result<()> {
@@ -20,9 +26,34 @@ async fn web_todo_list() -> Result<()> {
 	// -- CHECK
 	assert_eq!(200, resp.status(), "http status");
 
+	// extract response .data
+	let todos: Vec<Todo> = extract_body_data(resp)?;
+
+	// -- CHECK
+	assert_eq!(2, todos.len(), "number of todos");
+	assert_eq!(101, todos[0].id);
+	assert_eq!("todo 123", todos[0].title);
+	assert_eq!(TodoStatus::Open, todos[0].status);
+
 	Ok(())
 }
 
 /* #region  Web Test Utils */
+fn extract_body_data<D>(resp: Response<Bytes>) -> Result<D>
+where
+	for<'de> D: Deserialize<'de>,
+{
+	// parse the body as serde_json::Value
+	let body = from_utf8(resp.body())?;
+	let mut body: Value =
+		from_str(body).with_context(|| format!("Cannot parse resp.body to JSON. resp.body: '{}'", body))?;
 
+	// extract the data
+	let data = body["data"].take();
+
+	// deserialize the data to D
+	let data: D = from_value(data)?;
+
+	Ok(data)
+}
 /* #endregion */
